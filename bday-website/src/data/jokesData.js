e2ebc23e29ca442c6819041e9af35aebef9b7cb8
Ashrith Puppala <ashrithpuppala07@gmail.com>
2026-08-09 23:29:02 +0530

    Update jokesData.js

--- bday-website/src/data/jokesData.js
+++ bday-website/src/data/jokesData.js
@@ -1,8 +1,15 @@
 // Inside jokes shown when the flower is clicked - replace with your own
 export default [
-  "[Insert inside joke #1]",
-  "[Insert inside joke #2]",
-  "[Insert inside joke #3]",
-  "[Insert inside joke #4]",
-  "[Insert inside joke #5]"
+  "Filming the reel in Lulu",
+  "Bitching about fests and jrs",
+  "Sitting on the stage and crying after PHENOMENON",
+  "Putting bows on the guys and the barbie reel",
+  "Playing kings on your terrace",
+  "Holi in the middle of boards",
+  "Sundi saying that he wont take class cause you werent there",
+  "SIL Presentation",
+  "Your wonderful ceremonies team",
+  
 ];
+
+
