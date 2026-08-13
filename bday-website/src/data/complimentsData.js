8e9a5a47cf47a43e89846cf98e152ffdba8e5eff
Ashrith Puppala <ashrithpuppala07@gmail.com>
2026-08-14 00:04:01 +0530

    Update complimentsData.js

--- bday-website/src/data/complimentsData.js
+++ bday-website/src/data/complimentsData.js
@@ -1,9 +1,9 @@
 // Slot-machine compliments - replace with your own
 export default [
-  "[Compliment #1]",
-  "[Compliment #2]",
-  "[Compliment #3]",
-  "[Compliment #4]",
-  "[Compliment #5]",
-  "[Compliment #6]"
+  "Hats off to your dedication and constant efforts",
+  "Meeting you was one of the best things that has happened to us",
+  "Thank you for always being there for us and listening to us",
+  "You are a different level of comfort zone",
+  "How are you so decisive man (I am jealous)",
+  "Stay as innocent as you are"
 ];
